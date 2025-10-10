c0d236f39ff220eddc0956be1104fd8fd8f3c67c
Melissa <albusdumbledore4269@gmail.com>
2025-10-10 14:44:04 +0200

    feat(api): add CORS support and extend routes with PUT and list for articles

--- src/services/api-gateway/api-gateway.service.js
+++ src/services/api-gateway/api-gateway.service.js
@@ -1,3 +1,7 @@
+/*
+  PATH      /src/services/api-gateway/api-gateway.service.js
+*/
+
 const ApiGateway = require('moleculer-web')
 const { port, host } = require('../../config/application.config').moleculer
 
@@ -7,23 +11,29 @@ module.exports = {
   settings: {
     port,
     host,
+    cors: {
+      origin: '*', // Here to specify frontend
+      methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      exposedHeaders: [],
+      credentials: false,
+      maxAge: 3600
+    },
     routes: [{
-      path: '/',
+      path: '/api/v1',
       aliases: {
-        'POST articles': {
-          action: 'articles.create'
-        },
-        'GET articles/:slug': {
-          action: 'articles.getBySlug'
-        },
-        'DELETE articles/:slug': {
-          action: 'articles.deleteBySlug'
-        }
+        'POST /articles': 'articles.create',
+        'GET /articles': 'articles.list',
+        'GET /articles/:slug': 'articles.getBySlug',
+        'PUT /articles/:slug': 'article.updateBySlug',
+        'DELETE /articles/:slug': 'articles.deleteBySlug'
       },
       bodyParsers: {
-        json: true,
-        urlencoded: { extended: true }
+        json: true
       }
     }]
+  },
+  started () {
+    this.logger.info(`API Gateway started at http://${host}:${port}/api/v1`)
   }
 }
